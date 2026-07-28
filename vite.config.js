73a1f362774f9c6bc19b5b613282e2f035567c92
r27787558-pixel <r27787558@gmail.com>
2026-07-28 10:20:37 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // 设为相对路径，保证在 github.io 下正常加载静态资源
+  base: './',
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  }
 })
